6a0458377c5644cb964499e6a25e6e300c42e4a0
oksssllyy <mitya.tomilov.99@mail.ru>
2019-02-19 12:57:31 +0700

    file.cpp

--- file.cpp
+++ file.cpp
@@ -1,2 +1,1 @@
-int main() {
-for (int i = 0; i < 100; ++i) { std::cout << " First case " << std::endl; std::cout << " Second case " << std::endl; std::cout << " Third case " << std::endl; } } 
+int main() {for (int i = 0; i < 100; ++i) { std::cout << " First case " << std::endl; std::cout << " Second case " << std::endl; std::cout << " Third case " << std::endl; } } 
