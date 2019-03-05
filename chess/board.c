0e551e084616211eb2b0d5a719109024a0b3d1bb
oksssllyy <mitya.tomilov.99@mail.ru>
2019-03-05 13:01:18 +0700

    Create a new chess directory

---
+++ chess/board.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <board.h>
+
+    Board print_board()
+    {
+        for (i = 0; i < 8; i++)
+        {
+	    char cell = 0;
+	    case 0:
+	    case 7:
+		cell = 'R';
+		break;
+	    case 1:
+	    case 6:
+		cell = 'N';
+		break;
+	    case 2:
+	    case 5:
+		cell = 'B';
+		break;
+	    case 3:
+		cell = 'Q';
+		break;
+	    case 4:
+		cell = 'K';
+		break;
+	    default:
+		break;
+	}cell.board[0][i];
+
---
+++ chess/board.h
@@ -0,0 +1,10 @@
+#ifndef CHESSVIZ_BOARD_H
+#define CHESSVIZ_BOARD_H
+
+  typedef struct
+  {
+    char board[8][8];
+  }Board;
+
+#endif //CHESSVIZ_BOARD_
+
